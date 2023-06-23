41eb91b97dbb0d304a05287a688f8dab4f00e5f7
Kent Gibson <warthog618@gmail.com>
2023-06-24 00:32:14 +0800

    improved toggle_multiple_line_values

--- lib/examples/toggle_multiple_line_values.rs
+++ lib/examples/toggle_multiple_line_values.rs
@@ -5,31 +5,31 @@
 // Basic example of toggling multiple lines.
 
 use anyhow::Context;
-use gpiocdev::line::{Value, Values};
+use gpiocdev::line::Value;
 use gpiocdev::Request;
 use std::result::Result;
 use std::thread;
 use std::time::Duration;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut values = Values::default();
-    values.set(22, Value::Active).set(12, Value::Inactive);
+    let mut values = [(22, Value::Active), (12, Value::Inactive)]
+        .into_iter()
+        .collect();
+    // or equivalently...
+    //let mut values = Values::default();
+    //values.set(22, Value::Active).set(12, Value::Inactive);
 
     let req = Request::builder()
         .on_chip("/dev/gpiochip0")
         .with_consumer("toggle-multiple-line-values")
-        .with_line(22)
-        .as_output(Value::Active)
-        .with_line(12)
-        .as_output(Value::Inactive)
+        .with_output_lines(&values)
         .request()
         .context("Failed to request line")?;
 
     loop {
         println!("{:?}", values);
         thread::sleep(Duration::from_millis(500));
-        values.toggle(12);
-        values.toggle(22);
+        values.not();
         req.set_values(&values).context("Failed to set values")?;
     }
 }
