cd9ea43a654d77fe16576a1c35b041c4b5d2a441
Kent Gibson <warthog618@gmail.com>
2023-07-10 19:37:58 +0800

    rename benches

--- lib/benches/request.rs
+++ lib/benches/request.rs
@@ -4,7 +4,7 @@
 
 use criterion::{criterion_group, criterion_main, Bencher, Criterion};
 
-use gpiocdev::line::{Value, Values};
+use gpiocdev::line::Value;
 use gpiocdev::request::Request;
 use gpiocdev::AbiVersion;
 use gpiosim::Simpleton;
@@ -16,10 +16,10 @@ criterion_main!(v1_benches, v2_benches);
 #[cfg(feature = "uapi_v1")]
 fn v1_benchmarks(c: &mut Criterion) {
     use gpiocdev::AbiVersion::V1;
-    c.bench_function("uapi_v1 get one", |b| get_one(b, V1));
-    c.bench_function("uapi_v1 get ten", |b| get_ten(b, V1));
-    c.bench_function("uapi_v1 set one", |b| set_one(b, V1));
-    c.bench_function("uapi_v1 set ten", |b| set_ten(b, V1));
+    c.bench_function("uapi_v1 one input", |b| one_input(b, V1));
+    c.bench_function("uapi_v1 ten inputs", |b| ten_inputs(b, V1));
+    c.bench_function("uapi_v1 one output", |b| one_output(b, V1));
+    c.bench_function("uapi_v1 ten outputs", |b| ten_outputs(b, V1));
 }
 #[cfg(not(feature = "uapi_v1"))]
 fn v1_benchmarks(_c: &mut Criterion) {}
@@ -27,17 +27,16 @@ fn v1_benchmarks(_c: &mut Criterion) {}
 #[cfg(feature = "uapi_v2")]
 fn v2_benchmarks(c: &mut Criterion) {
     use gpiocdev::AbiVersion::V2;
-    c.bench_function("uapi_v2 get one", |b| get_one(b, V2));
-    c.bench_function("uapi_v2 get ten", |b| get_ten(b, V2));
-    c.bench_function("uapi_v2 set one", |b| set_one(b, V2));
-    c.bench_function("uapi_v2 set ten", |b| set_ten(b, V2));
+    c.bench_function("uapi_v2 one input", |b| one_input(b, V2));
+    c.bench_function("uapi_v2 ten inputs", |b| ten_inputs(b, V2));
+    c.bench_function("uapi_v2 one output", |b| one_output(b, V2));
+    c.bench_function("uapi_v2 ten outputs", |b| ten_outputs(b, V2));
 }
 #[cfg(not(feature = "uapi_v2"))]
-fn v1_benchmarks(_c: &mut Criterion) {}
+fn v2_benchmarks(_c: &mut Criterion) {}
 
-// determine time taken to get one line
 #[allow(unused_variables)]
-fn get_one(b: &mut Bencher, abiv: AbiVersion) {
+fn one_input(b: &mut Bencher, abiv: AbiVersion) {
     let s = Simpleton::new(10);
     let offset = 1;
 
@@ -45,16 +44,15 @@ fn get_one(b: &mut Bencher, abiv: AbiVersion) {
     builder.on_chip(s.dev_path());
     #[cfg(all(feature = "uapi_v1", feature = "uapi_v2"))]
     builder.using_abi_version(abiv);
-    let req = builder.with_line(offset).as_input().request().unwrap();
 
     b.iter(|| {
-        let value = req.value(offset);
+        let req = builder.with_line(offset).as_input().request().unwrap();
+        drop(req);
     });
 }
 
-// determine time taken to get ten lines
 #[allow(unused_variables)]
-fn get_ten(b: &mut Bencher, abiv: AbiVersion) {
+fn ten_inputs(b: &mut Bencher, abiv: AbiVersion) {
     let s = Simpleton::new(10);
     let offsets = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -62,17 +60,15 @@ fn get_ten(b: &mut Bencher, abiv: AbiVersion) {
     builder.on_chip(s.dev_path());
     #[cfg(all(feature = "uapi_v1", feature = "uapi_v2"))]
     builder.using_abi_version(abiv);
-    let req = builder.with_lines(&offsets).as_input().request().unwrap();
-    let mut values = Values::from_offsets(&offsets);
 
     b.iter(|| {
-        req.values(&mut values).unwrap();
+        let req = builder.with_lines(&offsets).as_input().request().unwrap();
+        drop(req);
     });
 }
 
-// determine time taken to set one line
 #[allow(unused_variables)]
-fn set_one(b: &mut Bencher, abiv: AbiVersion) {
+fn one_output(b: &mut Bencher, abiv: AbiVersion) {
     let s = Simpleton::new(10);
     let offset = 1;
 
@@ -80,20 +76,19 @@ fn set_one(b: &mut Bencher, abiv: AbiVersion) {
     builder.on_chip(s.dev_path());
     #[cfg(all(feature = "uapi_v1", feature = "uapi_v2"))]
     builder.using_abi_version(abiv);
-    let req = builder
-        .with_line(offset)
-        .as_output(Value::Active)
-        .request()
-        .unwrap();
 
     b.iter(|| {
-        req.set_value(offset, Value::Active).unwrap();
+        let req = builder
+            .with_line(offset)
+            .as_output(Value::Inactive)
+            .request()
+            .unwrap();
+        drop(req);
     });
 }
 
-// determine time taken to set multiple lines
 #[allow(unused_variables)]
-fn set_ten(b: &mut Bencher, abiv: AbiVersion) {
+fn ten_outputs(b: &mut Bencher, abiv: AbiVersion) {
     let s = Simpleton::new(10);
     let offsets = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -101,14 +96,13 @@ fn set_ten(b: &mut Bencher, abiv: AbiVersion) {
     builder.on_chip(s.dev_path());
     #[cfg(all(feature = "uapi_v1", feature = "uapi_v2"))]
     builder.using_abi_version(abiv);
-    let req = builder
-        .with_lines(&offsets)
-        .as_output(Value::Active)
-        .request()
-        .unwrap();
-    let values = Values::from_offsets(&offsets);
 
     b.iter(|| {
-        req.set_values(&values).unwrap();
+        let req = builder
+            .with_lines(&offsets)
+            .as_output(Value::Inactive)
+            .request()
+            .unwrap();
+        drop(req);
     });
 }
